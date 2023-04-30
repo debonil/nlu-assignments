bf43b19e5d32e38cac7c75500de07332d9dd5f1b
Debonil Ghosh <debonil.ghosh@gmail.com>
2023-04-30 11:48:05 +0530

    update to rasa

--- Assignment2/dsr_rasa_bot/actions/actions.py
+++ Assignment2/dsr_rasa_bot/actions/actions.py
@@ -17,7 +17,7 @@ from rasa_sdk.events import SlotSet
 print('loading actions:')
 
 
-def get_obj_by_subject(subject_name="Heraclio", properrty_name="NATIONALITY"):
+def get_person_details_by_rel(person_name="Heraclio", properrty_name="NATIONALITY"):
     response = None
     # Connection details
     uri = "bolt://localhost:7687"
@@ -29,7 +29,7 @@ def get_obj_by_subject(subject_name="Heraclio", properrty_name="NATIONALITY"):
     driver = GraphDatabase.driver(uri, auth=(
         username, password), encrypted=False)
     with driver.session(database=database) as session:
-        query = f" MATCH (n:Person{{name:'{subject_name}'}})-[:{properrty_name}] ->(r) RETURN r.name "
+        query = f" MATCH (n:Person)-[:{properrty_name}]->(r) WHERE n.name CONTAINS '{person_name}'  RETURN r.name "
         print(query)
         info = session.run(query)
         list_values = info.values()
@@ -41,7 +41,7 @@ def get_obj_by_subject(subject_name="Heraclio", properrty_name="NATIONALITY"):
     return response
 
 
-def get_subjects_by_objects(subject_name="Heraclio", properrty_name="NATIONALITY"):
+def get_all_persons_related_to(obj_related_to, relation_type):
     response = None
     # Connection details
     uri = "bolt://localhost:7687"
@@ -53,7 +53,7 @@ def get_subjects_by_objects(subject_name="Heraclio", properrty_name="NATIONALITY
     driver = GraphDatabase.driver(uri, auth=(
         username, password), encrypted=False)
     with driver.session(database=database) as session:
-        query = f" MATCH (n:Organization{{name:'{subject_name}'}})<-[:{properrty_name}]-(r) RETURN r.name "
+        query = f"  MATCH (n:Organization|Location|Date)<-[:{relation_type}]-(r:Person) WHERE n.name CONTAINS '{obj_related_to}' RETURN r.name  "
         print(query)
         info = session.run(query)
         list_values = info.values()
@@ -89,6 +89,30 @@ def get_all_obj_by_subject(subject_name):
     return response
 
 
+def does_person_exists(person_name):
+    response = False
+    # Connection details
+    uri = "bolt://localhost:7687"
+    username = "etl_user"
+    password = "etl_user123"
+    database = "knowledgenet"   # database name
+
+    # Connect to the Neo4j database
+    driver = GraphDatabase.driver(uri, auth=(
+        username, password), encrypted=False)
+    with driver.session(database=database) as session:
+        query = f"MATCH (n:Person) WHERE n.name CONTAINS '{person_name}' RETURN n.name"
+        print(query)
+        info = session.run(query)
+        list_values = info.values()
+        # print(type(list_values))
+        print(list_values)
+        if len(list_values) > 0:
+            response = True
+
+    return response
+
+
 class ActionDescPerson(Action):
 
     def name(self) -> Text:
@@ -105,7 +129,7 @@ class ActionDescPerson(Action):
         query = intent[13:]
 
         print(f'intent={intent}, entity_value={person}, query={query}')
-        result = get_obj_by_subject(person, query)
+        result = get_person_details_by_rel(person, query)
         print(f'response ==> {result}')
 
         if person != None:
@@ -157,6 +181,36 @@ class ActionPersonAllDetails(Action):
         return [SlotSet("PERSON", person)]
 
 
+class ActionPersonInKnowledgeOrNot(Action):
+
+    def name(self) -> Text:
+        return "action_person_known_or_not"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        intent = tracker.latest_message['intent'].get('name')
+        person = tracker.get_slot('PERSON')
+        if len(tracker.latest_message['entities']) > 0:
+            person = tracker.latest_message['entities'][0]['value']
+
+        print(f'intent={intent}, entity_value={person}')
+        result = does_person_exists(person)
+        print(f'response ==> {result}')
+
+        if person != None:
+            if result != None:
+                dispatcher.utter_message(response="utter_person_known",
+                                         person=person)
+            else:
+                dispatcher.utter_message(response="utter_person_unknown",
+                                         person=person)
+        else:
+            dispatcher.utter_message(
+                text="Sorry could not get whome you are refering.. can you refrase your query with person name? ")
+        return [SlotSet("PERSON", person)]
+
+
 class ActionPersonAllPersonInRelation(Action):
 
     def name(self) -> Text:
@@ -174,7 +228,7 @@ class ActionPersonAllPersonInRelation(Action):
 
         print(f'intent={intent}, objectText={objectText}, query={query}')
 
-        result = get_obj_by_subject(objectText, query)
+        result = get_all_persons_related_to(objectText, query)
         print(f'response ==> {result}')
 
         if result != None:
